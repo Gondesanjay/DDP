bcea98b7470d8b212b2c45ee435e9374e3cc41d3
Gondesanjay <doctor031102@gmail.com>
2024-12-04 15:43:38 +0700

    Add files via upload

---
+++ InfoGempa.py
@@ -0,0 +1,13 @@
+from Gempa import *
+
+Gempa1 = Gempa('Banten', 1.2)
+Gempa2 = Gempa('Palu', 6.1)
+Gempa3 = Gempa('Cianjur', 5.6)
+Gempa4 = Gempa('Jayapura', 3.3)
+Gempa5 = Gempa('Garut', 4.0)
+
+Gempa1.dampak()
+Gempa2.dampak()
+Gempa3.dampak()
+Gempa4.dampak()
+Gempa5.dampak()
